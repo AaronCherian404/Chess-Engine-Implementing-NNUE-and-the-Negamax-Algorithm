abd0b5ba71506a8978971f75aeaa460aa4e7f58e
amitozSingh-projects <141937853+amitozSingh-projects@users.noreply.github.com>
2023-08-27 13:54:45 +0530

    Added necessary board assets
    Making the code for making the board and rendering pieces modular.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#This is the main file that calls and makes the other components of the engine work.
+"""
+This is the main file that calls and makes the other components of the engine work.
+
+Main driver file
+Handling user input
+"""
+import pygame as game
+import sys
+from multiprocessing import Process, Queue
+
+from User_Interface.board import board
+
+def main():
+    
+    board.init_board()
+
+    
+
