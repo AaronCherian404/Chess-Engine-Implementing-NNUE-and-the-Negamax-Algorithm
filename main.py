fb2bba87a40c6ff0607e76ffac56c32c6b6e00e3
amitozSingh-projects <141937853+amitozSingh-projects@users.noreply.github.com>
2023-08-31 15:36:55 +0530

    Tried rendring pieces
    Still need to work on the backend to make the pieces move and work

--- main.py
+++ main.py
@@ -8,11 +8,30 @@ import pygame as game
 import sys
 from multiprocessing import Process, Queue
 
-from User_Interface.board import board
+BOARD_WIDTH = BOARD_HEIGHT = 512
+MOVE_LOG_PANEL_WIDTH = 250
+MOVE_LOG_PANEL_HEIGHT = BOARD_HEIGHT
+DIMENSION = 8
+SQUARE_SIZE = BOARD_HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+def loadImages():
+    """
+    Initialize a global directory of images.
+    This will be called exactly once in the main.
+    """
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("D:\singh\Desktop\chess-engine-master\chess\images\\" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
 
 def main():
+    """
+    Main driver of the code.
+    Handles the user input and updates the graphics.
+    """
+    game.init()
+    screen = game.display.set_mode((BOARD_WIDTH + MOVE_LOG_PANEL_WIDTH, BOARD_HEIGHT))
+    clock = game.time.Clock()
+    screen.fill(game.Color("white"))
     
-    board.init_board()
-
-    
-
